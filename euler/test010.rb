1e06dfd28194a25f8cef460f7855d8b9eb323701
snito <snito@appirits.com>
2015-04-02 13:28:50 +0900

    問10を作成

--- euler/test010.rb
+++ euler/test010.rb
@@ -1,4 +1,29 @@
 #10以下の素数の和は2 + 3 + 5 + 7 = 17である.
 #200万以下の全ての素数の和を計算しなさい
-#
+require 'prime'
 
+class SumPrimesCalculator
+  def initialize(calc_limit)
+    @calc_limit = calc_limit
+  end
+
+  def sum
+    primes = pickup_prime
+
+    sum = 0
+    primes.each {|prime| sum += prime}
+    sum
+  end
+
+  private
+  def pickup_prime
+    primes = []
+    (1..@calc_limit).each do |num|
+      primes << num if Prime.prime? num
+    end
+    primes
+  end
+end
+
+calc = SumPrimesCalculator.new(1000)
+p calc.sum
