d73d180751eb66628deb78dd42e66128a95b31c7
snito <snito@appirits.com>
2015-04-02 18:59:36 +0900

    問5をある程度、記述

--- euler/test005.rb
+++ euler/test005.rb
@@ -3,27 +3,23 @@
 #では、1 から 20 までの整数全てで割り切れる
 #数字の中で最小の値はいくらになるか。
 
-num = 20
-sum = 20
-ok_ary = []
+class LowestCommonCalculator
+   def initialize(target_range)
+     @target_range = target_range
+     calc
+   end
 
-while  num >=  2
-    if sum % (num - 1) != 0
-        sum = sum * (num - 1) 
+   def print_answer
+
+   end
+
+  private
+  def calc
+    for i in @target_range
+      # TODO
     end
-    num -= 1
+  end
 end
 
-(1..20).to_a.each do |m|
-    if sum % m == 0
-        puts "OK"
-        ok_ary << m
-    else 
-        puts "NG"
-    end
-end
-
-puts sum
-print ok_ary
-
-
+calc = LowestCommonCalculator.new(1..20)
+calc.print_answer
